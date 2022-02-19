1ec6620ad159fe86d4bc1f8b4e7e128307e61cff
Trần Tuấn <trantuan@iMac.local>
2022-02-19 20:31:36 +0700

    init layout

--- application/src/common/rootReducer.js
+++ application/src/common/rootReducer.js
@@ -4,14 +4,12 @@ import historyCommon from "./historyCommon";
 import {reducer as CrudReducer} from "./crud/reducer"
 import {reducer as AuthReducer} from "../features/Auth/redux/reducer"
 import {reducer as HomeReducer} from "../features/Home/redux/reducer"
-import {reducer as UserReducer} from "../features/Users/redux/reducer"
 
 const reducerMap = {
     router: connectRouter(historyCommon),
     crud  : CrudReducer,
     auth  : AuthReducer,
     home  : HomeReducer,
-    user  : UserReducer,
 };
 
 export default combineReducers(reducerMap);
